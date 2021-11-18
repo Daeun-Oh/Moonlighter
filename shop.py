45adf55713cf722dbf2991a26a33dbe3748b1e15
Daeun-Oh <annsomde@naver.com>
2021-11-18 09:14:14 +0900

    상점 클래스 수정

--- shop.py
+++ shop.py
@@ -3,6 +3,14 @@ from pico2d import *
 class Shop:
     def __init__(self):
         self.image = load_image('Overworld.png')
+        self.x, self.y = 300, 300
+
+    def get_bb(self):
+        return self.x - 40, self.y - 16, self.x + 40, self.y + 24
+
+    def update(self):
+        pass
 
     def draw(self):
-        self.image.clip_draw(288, 120, 80, 100, 300, 300)
+        self.image.clip_draw(288, 120, 80, 96, self.x, self.y)
+        # draw_rectangle(*self.get_bb())
